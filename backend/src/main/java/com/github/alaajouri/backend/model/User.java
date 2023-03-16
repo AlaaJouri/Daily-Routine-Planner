0be6ce92bf58fdc5696b2ff5b3b6cad26e1309ac
alaajouri <alaa.joryi@hotmail.com>
2023-03-16 16:49:48 +0100

    Add variable mongo url

--- backend/src/main/java/com/github/alaajouri/backend/model/User.java
+++ backend/src/main/java/com/github/alaajouri/backend/model/User.java
@@ -1,10 +1,6 @@
 package com.github.alaajouri.backend.model;
 
-import org.springframework.data.mongodb.core.mapping.Document;
-
-@Document("user")
 public record User(
-        // @Id
         String id,
         String name,
         String geschlecht,
