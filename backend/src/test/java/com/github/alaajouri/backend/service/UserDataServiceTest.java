f750085034bb1893dd8f1e7a0545fc86c589de1a
alaajouri <alaa.joryi@hotmail.com>
2023-03-17 18:11:51 +0100

    Add UserData

---
+++ backend/src/test/java/com/github/alaajouri/backend/service/UserDataServiceTest.java
@@ -0,0 +1,44 @@
+package com.github.alaajouri.backend.service;
+
+import com.github.alaajouri.backend.model.UserData;
+import com.github.alaajouri.backend.repository.UserDataRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import static org.mockito.Mockito.*;
+
+
+class UserDataServiceTest {
+
+    UserDataService userDataService;
+    UserDataRepository userDataRepository;
+    IdGenerator idGenerator;
+
+    @BeforeEach
+    public void setUp() {
+        userDataRepository = mock(UserDataRepository.class);
+        idGenerator = mock(IdGenerator.class);
+        userDataService = new UserDataService(userDataRepository, idGenerator);
+    }
+
+    @Test
+    void addUserData() {
+
+
+        //GIVEN
+        UserData expectedUserData = new UserData("1", "Alaa", "women", "160");
+        when(idGenerator.generateID()).thenReturn("1");
+        when(userDataRepository.save(expectedUserData)).thenReturn(expectedUserData);
+
+
+        //WHEN
+        UserData userData = userDataService.addUserData(expectedUserData);
+
+        //THEN
+        verify(userDataRepository).save(expectedUserData);
+        assertEquals(expectedUserData, userData);
+    }
+}
