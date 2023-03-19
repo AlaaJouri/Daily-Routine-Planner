54e527b6252ee1c23c63a46b674cc8c2713a6220
alaajouri <alaa.joryi@hotmail.com>
2023-03-20 00:35:58 +0100

    Add UserData CSS

--- frontend/src/component/AddUserData.tsx
+++ frontend/src/component/AddUserData.tsx
@@ -1,6 +1,7 @@
 import {UserData} from "../model/UserData";
 import {ChangeEvent, useState} from "react";
 import "./AddUserData.css";
+
 type AddUserDataProps = {
     addUserData: (userDataToAdd: UserData) => void,
 }
@@ -94,70 +95,74 @@ export default function AddUserData(props: AddUserDataProps) {
     return (
 
 
-    <div className="opac-layer">
-        <div>
-            <h1 id="title">Profile</h1>
-            <p id="description">Planen Sie Ihren Tag</p>
-            <hr />
-        </div>
-        <form id="survey-form" className="container">
-            <div className="item0">
-                <label htmlFor="name">Enter your name:</label>
-                <br />
-                <input value={userDataToAdd.name} onChange={handleChangeName} placeholder={"name"}/>
-
-                <br />
+        <div className="Profile">
+            <div>
+                <h1 id="title">Profile</h1>
+                <p id="description">Planen Sie Ihren Tag</p>
+                <hr/>
             </div>
-            <div className="item1">
-                <label htmlFor="email">Enter your email address:</label>
-                <br />
-                <input value={userDataToAdd.gender} onChange={handleChangeGender} placeholder={"gender"}/>
+            <form id="survey-form" className="container">
+                <div className="item0">
+                    <label htmlFor="name">Name</label>
+                    <br/>
+                    <input value={userDataToAdd.name} onChange={handleChangeName}/>
 
-                <br />
-            </div>
-            <div className="item2">
-                <label htmlFor="email">Enter your email address:</label>
-                <br />
-                <input value={userDataToAdd.weight} onChange={handleChangeWeight} placeholder={"weight"}/>
+                    <br/>
+                </div>
+                <div className="item1">
+                    <label htmlFor="Geschlecht">Geschlecht</label>
+                    <br/>
+                    <input value={userDataToAdd.gender} onChange={handleChangeGender}/>
 
-                <br />
-            </div> <div className="item3">
-            <label htmlFor="email">Enter your email address:</label>
-            <br />
-            <input value={userDataToAdd.weightGoal} onChange={handleChangeWeightGoal} placeholder={"weightGoal"}/>
+                    <br/>
+                </div>
+                <div className="item2">
+                    <label htmlFor="Gewicht">Gewicht</label>
+                    <br/>
+                    <input value={userDataToAdd.weight} onChange={handleChangeWeight}/>
+
+                    <br/>
+                </div>
+                <div className="item3">
+                    <label htmlFor="Zielgewicht">Zielgewicht</label>
+                    <br/>
+                    <input value={userDataToAdd.weightGoal} onChange={handleChangeWeightGoal}/>
 
 
-            <br />
-        </div> <div className="item4">
-            <label htmlFor="email">Enter your email address:</label>
-            <br />
-            <input value={userDataToAdd.sleepTimeTarget} onChange={handleChangeSleepTimeTarget} placeholder={"sleepTimeTarget"}/>
+                    <br/>
+                </div>
+                <div className="item4">
+                    <label htmlFor="Schlafzeitziel">Schlafzeitziel</label>
+                    <br/>
+                    <input value={userDataToAdd.sleepTimeTarget} onChange={handleChangeSleepTimeTarget}/>
 
-            <br />
-        </div> <div className="item5">
-            <label htmlFor="email">Enter your email address:</label>
-            <br />
-            <input value={userDataToAdd.trainingTimeGoal} onChange={handleChangeTrainingTimeGoal} placeholder={"trainingTimeGoal"}/>
+                    <br/>
+                </div>
+                <div className="item5">
+                    <label htmlFor="TrainingszeitZiel">Trainingszeit Ziel</label>
+                    <br/>
+                    <input value={userDataToAdd.trainingTimeGoal} onChange={handleChangeTrainingTimeGoal}/>
 
-            <br />
-        </div> <div className="item6">
-            <label htmlFor="email">Enter your email address:</label>
-            <br />
-            <input value={userDataToAdd.stepTarget} onChange={handleChangeStepTarget} placeholder={"stepTarget"}/>
+                    <br/>
+                </div>
+                <div className="item6">
+                    <label htmlFor="SchrittZiel">SchrittZiel</label>
+                    <br/>
+                    <input value={userDataToAdd.stepTarget} onChange={handleChangeStepTarget}/>
 
-            <br />
-        </div>
-            <div className="item7">
-                <label htmlFor="email">Enter your email address:</label>
-                <br />
-                <input value={userDataToAdd.caloriesBurnedTarget} onChange={handleChangeCaloriesBurnedTarget}
-                       placeholder={"caloriesBurnedTarget"}/>
+                    <br/>
+                </div>
+                <div className="item7">
+                    <label htmlFor="KalorienverbrauchZiel">KalorienverbrauchZiel</label>
+                    <br/>
+                    <input value={userDataToAdd.caloriesBurnedTarget} onChange={handleChangeCaloriesBurnedTarget}
+                           placeholder={"caloriesBurnedTarget"}/>
 
-                <br />
-            </div>
-            <button className="item8" onClick={handleClickAddUserData}>Add your Data</button>
+                    <br/>
+                </div>
+                <button className="item8" onClick={handleClickAddUserData}>Speichern</button>
 
-        </form>
+            </form>
         </div>
     );
 }
