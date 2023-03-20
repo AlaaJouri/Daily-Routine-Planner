c820ac03436e74a8b59f1bfb9d9848e716d07406
alaajouri <alaa.joryi@hotmail.com>
2023-03-20 11:27:34 +0100

    Add DTOUserData

--- backend/src/main/java/com/github/alaajouri/backend/controller/UserDataController.java
+++ backend/src/main/java/com/github/alaajouri/backend/controller/UserDataController.java
@@ -3,14 +3,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import com.github.alaajouri.backend.model.UserData;
 import com.github.alaajouri.backend.service.UserDataService;
-
+import com.github.alaajouri.backend.model.DTOUserData;
 @RestController
 @RequestMapping("api")
 @RequiredArgsConstructor
 public class UserDataController {
     private final UserDataService userDataService;
     @PostMapping("/userdata")
-    public UserData addWorkout(@RequestBody UserData userData) {
-        return userDataService.addUserData(userData);
+    public UserData addWorkout(@RequestBody UserData DTOUserData) {
+        return userDataService.addUserData(DTOUserData);
     }
 }
