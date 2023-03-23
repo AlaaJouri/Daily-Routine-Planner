95ad41c7097db9d8d6038ac0ffd12031f6507090
alaajouri <alaa.joryi@hotmail.com>
2023-03-23 11:24:06 +0100

    Add MongoUserDetailsService Test

---
+++ backend/src/test/java/com/github/alaajouri/backend/service/MongoUserDetailsServiceTest.java
@@ -0,0 +1,54 @@
+package com.github.alaajouri.backend.service;
+
+import com.github.alaajouri.backend.model.MongoUser;
+import com.github.alaajouri.backend.model.UserData;
+import com.github.alaajouri.backend.repository.MongoUserRepository;
+import com.github.alaajouri.backend.repository.UserDataRepository;
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
+
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MongoUserDetailsServiceTest {
+    PasswordEncoder passwordEncoder;
+    MongoUserDetailsService mongoUserDetailsService;
+    MongoUserRepository mongoUserRepository;
+    IdGenerator idGenerator;
+    MongoUser mongoUser;
+
+    @BeforeEach
+    void setUp() {
+        mongoUserRepository = mock(MongoUserRepository.class);
+        passwordEncoder = mock(PasswordEncoder.class);
+        idGenerator = mock(IdGenerator.class);
+        mongoUserDetailsService = new MongoUserDetailsService(mongoUserRepository);
+        mongoUser = new MongoUser("1", "username", "123", "BASIC");
+    }
+
+    @Test
+    void loadUserByUsername() {
+        //GIVEN
+        when(mongoUserRepository.findByUsername("username")).thenReturn(Optional.of(mongoUser));
+        //WHEN
+        UserDetails actual = mongoUserDetailsService.loadUserByUsername("username");
+        UserDetails expected = new User(mongoUser.username(), mongoUser.password(),
+                List.of(new SimpleGrantedAuthority(("ROLE_" + mongoUser.role()))));
+        //THEN
+        verify(mongoUserRepository).findByUsername("username");
+        Assertions.assertEquals(expected, actual);
+    }
+}
